ff9b325016943a3f3332007ce0a9924ab4d1894e
kongxy <kxy_1@163.com>
2020-10-18 16:35:53 +0800

    persion add属性更改

--- TestMoudule/src/com/kxy/module/Persion.java
+++ TestMoudule/src/com/kxy/module/Persion.java
@@ -5,6 +5,7 @@ public class Persion {
     private double height1;
     private int height;
     public double weight;
+    public String address;
     public static void main(String[] args) {
         int a;
 
