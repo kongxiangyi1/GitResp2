fbcaf6047f2bcac66a3e88f025b48c4f46b6ceda
kongxy <kxy_1@163.com>
2020-10-18 16:08:21 +0800

    persion push

--- TestMoudule/src/com/kxy/module/Persion.java
+++ TestMoudule/src/com/kxy/module/Persion.java
@@ -2,7 +2,7 @@ package com.kxy.module;
 
 public class Persion {
     private int age;
-
+    private double height1;
     private int height;
     public static void main(String[] args) {
         int a;
