d829167f587147c5875024b8915ec3003281656e
kongxy <kxy_1@163.com>
2020-10-18 16:36:57 +0800

    更改person属性

--- TestMoudule/src/com/kxy/module/Persion.java
+++ TestMoudule/src/com/kxy/module/Persion.java
@@ -5,6 +5,7 @@ public class Persion {
     private double height1;
     private int height;
     public double weight;
+    public boolean sex;
     public static void main(String[] args) {
         int a;
 
