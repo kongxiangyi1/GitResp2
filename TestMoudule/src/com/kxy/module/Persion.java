28065ed56b6b2d177c79e80a31137597d812e96e
kongxy <kxy_1@163.com>
2020-10-18 16:31:51 +0800

    persion name属性更改

--- TestMoudule/src/com/kxy/module/Persion.java
+++ TestMoudule/src/com/kxy/module/Persion.java
@@ -4,6 +4,7 @@ public class Persion {
     private int age;
     private double height1;
     private int height;
+    private String name;
     public static void main(String[] args) {
         int a;
 
